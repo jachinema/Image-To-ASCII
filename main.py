17c51e97d29013b9682691da439a2c91832f7320
jachinema <99571020+jachinema@users.noreply.github.com>
2022-03-08 00:56:08 -0600

    Major Update, see extended description
    Now also renders an image containing all of the ascii for a full view (higher res images could not even be seen properly with full zoom out in notepad++.)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import cv2 as cv
 from tkinter import Tk, filedialog, Button, Label
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+RELATIVE_PATH = os.getcwd()
 
 
 def average(nums):
@@ -16,34 +20,49 @@ def get_ascii(brightness):
         base <= brightness < base + (increment * 1): "▓",
         base + (increment * 1) <= brightness < base + (increment * 2): "▒",
         base + (increment * 2) <= brightness < base + (increment * 3): "░",
-        base + (increment * 3) <= brightness < base + (increment * 4): "::::",
-        base + (increment * 4) <= brightness < base + (increment * 5): "....",
-        brightness >= base + (increment * 5): "    "
+        base + (increment * 3) <= brightness < base + (increment * 4): ":",
+        base + (increment * 4) <= brightness < base + (increment * 5): ".",
+        brightness >= base + (increment * 5): " "
     }
 
     return table[True]
 
 
 def make_ascii(image):
-    
     image = cv.imread(image)
-    string = '\t\t\t\t\t\t\t\t'
+    string = ''
+
+    x, y, _ = image.shape
+
+    outimg = Image.new('L', (y*6, x*9), 255)
+    draw = ImageDraw.Draw(outimg)
+
+    height = len(image)
+    width = len(image[0])
+
     for row in range(0, height):
         for col in range(0, width):
             section = average(image[row, col])
             string += get_ascii(section)
-            
         string += '\n'
-        string += '\n\t\t\t\t\t\t\t\t'
 
     with open('output.txt', 'w', encoding='utf-8') as f:
         f.write(string)
+
+    draw.multiline_text(
+        (0, 0),
+        string,
+        font=ImageFont.truetype(RELATIVE_PATH+'\courbd.ttf', 10),
+        fill=0,
+        spacing=0
+    )
+    outimg = outimg.save("output.png")
 
 
 def run(master):
     path = filedialog.askopenfilename()
     make_ascii(path)
-    Label(master, text='Done! You can find the output.txt in this directory.', font=('Arial', 24)).pack()
+    Label(master, text='Done! You can find the output png and txt in this directory.', font=('Arial', 24)).pack()
 
 
 if __name__ == '__main__':
